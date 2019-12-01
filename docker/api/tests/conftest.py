f002f7f21132a6d3237ea14b04c3c1a5596c65bc
Tom <tom@scrabblehero.com>
2019-12-01 07:30:34 -0800

    Add test fixtures

--- docker/api/tests/conftest.py
+++ docker/api/tests/conftest.py
@@ -1,13 +1,43 @@
+import os
 import pytest
+import sqlalchemy
 
 from app import create_app
+from app.models import db as _db
+
+
+
+@pytest.fixture(scope='session')
+def dbconn():
+    engine = sqlalchemy.engine.create_engine(os.environ['DATABASE_URL'])
+    conn = engine.connect()
+    conn.execution_options(isolation_level="AUTOCOMMIT").execute('drop database if exists testing;')
+    conn.execution_options(isolation_level="AUTOCOMMIT").execute('create database testing;')
+    return dbconn
 
 
 @pytest.fixture(scope="session")
-def app():
+def app(dbconn):
     return create_app(environment="testing")
 
 
 @pytest.fixture(scope="session")
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture(scope="session")
+def database(app, client):
+    with app.app_context():
+        _db.drop_all()
+        _db.create_all()
+        yield _db
+        _db.drop_all()
+
+
+@pytest.fixture()
+def db(database):
+    database.session.rollback()
+    database.session.begin(subtransactions=True)
+    yield database
+    database.session.rollback()
--- docker/api/tests/test_dbconn.py
+++ docker/api/tests/test_dbconn.py
@@ -1,6 +1,14 @@
 import os
+import pytest
 
 from sqlalchemy import create_engine
+
+from app.models import db, Word
+
+
+@pytest.fixture
+def mockword():
+    return "Testing!"
 
 
 def test_connection_url():
@@ -14,3 +22,18 @@ def test_connection_with_test_app(app):
     db_url = app.config['DATABASE_URL']
     assert 'production' not in db_url
     assert app.config['DATABASE_URL'] == app.config['SQLALCHEMY_DATABASE_URI']
+
+
+def test_connection_with_db(db):
+    assert type(db.engine.table_names()) == list
+
+
+def test_transactions_are_isolated_first_txn(db, mockword):
+    word = Word(id=mockword)
+    db.session.add(word)
+    db.session.commit()
+    assert Word.query.get(mockword) is not None
+
+
+def test_transactions_are_isolated_second_txn(db, mockword):
+    assert Word.query.get(mockword) is None
