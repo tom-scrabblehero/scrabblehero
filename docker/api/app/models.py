2fa9934a55b05ad91de8b4fde2fd4bf9b7639bf4
Tom <tom@scrabblehero.com>
2019-11-30 20:25:34 -0800

    WIP add 'Word' model

--- docker/api/app/models.py
+++ docker/api/app/models.py
@@ -1,3 +1,14 @@
+import datetime as dt
+
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
+
+
+class TimestampMixin(object):
+    created_at = db.Column(db.DateTime, default=dt.datetime.now)
+    updated_at = db.Column(db.DateTime, default=dt.datetime.now, onupdate=dt.datetime.now)
+
+class Word(db.Model):
+    id = db.Column(db.String(), primary_key=True)
