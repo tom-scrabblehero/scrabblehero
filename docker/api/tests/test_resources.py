fc31cbcdc4fb65cf2e529f9cae66046aca139e41
tjwaterman99@gmail.com <tjwaterman99@gmail.com>
2019-12-11 20:23:02 -0800

    Update tests

--- docker/api/tests/test_resources.py
+++ docker/api/tests/test_resources.py
@@ -3,21 +3,21 @@
 def test_index(client):
     resp = client.get('/')
     assert resp.status_code == 200
-    assert resp.json['healthy'] == True
+    assert resp.json['data']['healthy'] == True
 
 
 def test_words_index(client, word):
     resp = client.get('/words')
     assert resp.status_code == 200
-    assert type(resp.json) == list
-    assert resp.json[0].get('id') == word.id
+    assert type(resp.json['data']) == list
+    assert resp.json['data'][0].get('id') == word.id
 
 
 def test_words_detail(client, word):
     resp = client.get(f'/words/{word.id}')
     assert resp.status_code == 200
     assert type(resp.json) == dict
-    assert resp.json['id'] == word.id
+    assert resp.json['data']['id'] == word.id
 
     resp = client.get(f'/words/doesnotexist')
     assert resp.status_code == 404
